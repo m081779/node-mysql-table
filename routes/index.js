e68f16cda58ed3583a0cec5cdfe0f4c118bf3e96
m081779 <marcoprincipio@hotmail.com>
2018-05-16 13:54:02 -0400

    trying to resolve issues with heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,10 @@ const connection = require('../config/connection.js');
 
 router.get('/', (req, res) => {
   let query = "SELECT * FROM table_data";
-  connection.query('INSERT INTO table_data(first_name, last_name, home) VALUES("Rose", "Tyler", "Earth"),("Zoe", "Herriot", "Space Station W3"),("Jo", "Grant", "Earth"),("Leela", null, "Unspecified"),("Romana", null, "Gallifrey"),("Clara", "Oswald", "Earth"),("Adric", "Null", "Alzarius"),("Susan", "Foreman", "Gallifrey");', (err,result)=>{
-    connection.query(query, (err, result) => {
-      if (err) throw err;
-      res.render('index', {results: result});
-    });
-  })
-
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    res.render('index', {results: result});
+  });
 });
 
 module.exports = router;
