d4c2273820003e3b406ef5f1be269a9c969b2e78
m081779 <marcoprincipio@hotmail.com>
2018-05-16 13:49:39 -0400

    trying to resolve issues with heroku deployments

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const connection = require('../config/connection.js');
 
 router.get('/', (req, res) => {
   let query = "SELECT * FROM table_data";
-  connection.query("DROP DATABASE if exists table_data CREATE DATABASE table_data USE table_data CREATE TABLE table_data(id INT AUTO_INCREMENT PRIMARY KEY,first_name VARCHAR(20),last_name VARCHAR(20), home VARCHAR(20));", (err,result)=>{
+  connection.query("CREATE TABLE table_data(id INT AUTO_INCREMENT PRIMARY KEY,first_name VARCHAR(20),last_name VARCHAR(20), home VARCHAR(20));", (err,result)=>{
     connection.query(query, (err, result) => {
       if (err) throw err;
       res.render('index', {results: result});
